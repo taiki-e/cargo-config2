203eceb2d3c0a9ade67f7d40bd8591e9c64559dc
Taiki Endo <te316e89@gmail.com>
2024-01-27 02:03:52 +0900

    codegen: Cleanup

--- src/gen/assert_impl.rs
+++ src/gen/assert_impl.rs
@@ -4,56 +4,49 @@
 // It is not intended for manual editing.
 
 #![cfg_attr(rustfmt, rustfmt::skip)]
-#![allow(clippy::std_instead_of_alloc, clippy::std_instead_of_core)]
-#[allow(dead_code)]
+#![allow(
+    dead_code,
+    unused_macros,
+    clippy::std_instead_of_alloc,
+    clippy::std_instead_of_core,
+)]
 fn assert_send<T: ?Sized + Send>() {}
-#[allow(dead_code)]
 fn assert_sync<T: ?Sized + Sync>() {}
-#[allow(dead_code)]
 fn assert_unpin<T: ?Sized + Unpin>() {}
-#[allow(dead_code)]
 fn assert_unwind_safe<T: ?Sized + std::panic::UnwindSafe>() {}
-#[allow(dead_code)]
 fn assert_ref_unwind_safe<T: ?Sized + std::panic::RefUnwindSafe>() {}
-#[allow(unused_imports)]
-use core::marker::PhantomPinned;
 /// `Send` & `!Sync`
-#[allow(dead_code)]
 struct NotSync(core::cell::UnsafeCell<()>);
+/// `!Send` & `Sync`
+struct NotSend(std::sync::MutexGuard<'static, ()>);
 /// `!Send` & `!Sync`
-#[allow(dead_code)]
 struct NotSendSync(*const ());
+/// `!Unpin`
+struct NotUnpin(core::marker::PhantomPinned);
 /// `!UnwindSafe`
-#[allow(dead_code)]
 struct NotUnwindSafe(&'static mut ());
 /// `!RefUnwindSafe`
-#[allow(dead_code)]
 struct NotRefUnwindSafe(core::cell::UnsafeCell<()>);
-#[allow(unused_macros)]
 macro_rules! assert_not_send {
     ($ty:ty) => {
         static_assertions::assert_not_impl_all!($ty : Send);
     };
 }
-#[allow(unused_macros)]
 macro_rules! assert_not_sync {
     ($ty:ty) => {
         static_assertions::assert_not_impl_all!($ty : Sync);
     };
 }
-#[allow(unused_macros)]
 macro_rules! assert_not_unpin {
     ($ty:ty) => {
         static_assertions::assert_not_impl_all!($ty : Unpin);
     };
 }
-#[allow(unused_macros)]
 macro_rules! assert_not_unwind_safe {
     ($ty:ty) => {
         static_assertions::assert_not_impl_all!($ty : std::panic::UnwindSafe);
     };
 }
-#[allow(unused_macros)]
 macro_rules! assert_not_ref_unwind_safe {
     ($ty:ty) => {
         static_assertions::assert_not_impl_all!($ty : std::panic::RefUnwindSafe);
@@ -257,12 +250,12 @@ const _: fn() = || {
     assert_ref_unwind_safe::<crate::resolve::TargetTriple>();
     assert_send::<crate::value::Value<()>>();
     assert_send::<crate::value::Value<NotSync>>();
-    assert_not_send!(crate::value::Value<NotSendSync>);
+    assert_not_send!(crate::value::Value<NotSend>);
     assert_sync::<crate::value::Value<()>>();
+    assert_sync::<crate::value::Value<NotSend>>();
     assert_not_sync!(crate::value::Value<NotSync>);
-    assert_not_sync!(crate::value::Value<NotSendSync>);
     assert_unpin::<crate::value::Value<()>>();
-    assert_not_unpin!(crate::value::Value<PhantomPinned>);
+    assert_not_unpin!(crate::value::Value<NotUnpin>);
     assert_unwind_safe::<crate::value::Value<()>>();
     assert_not_unwind_safe!(crate::value::Value<NotUnwindSafe>);
     assert_ref_unwind_safe::<crate::value::Value<()>>();
