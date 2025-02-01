55cd34b61382dd86ef4408d860da696dee1ac5fb
Taiki Endo <te316e89@gmail.com>
2025-02-01 10:14:26 +0900

    tests: Update size test

--- tests/test.rs
+++ tests/test.rs
@@ -14,7 +14,7 @@ use self::helper::*;
 // Test the size of public types. This is not intended to keep a specific size and
 // is intended to be used only as a help in optimization.
 #[test]
-#[cfg_attr(any(not(target_pointer_width = "64"), miri), ignore)] // We set -Z randomize-layout for Miri.
+#[cfg_attr(any(not(target_pointer_width = "64"), miri, careful), ignore)] // We set -Z randomize-layout for Miri/cargo-careful.
 fn size() {
     if cfg!(windows) {
         // TODO: size is large for some reason.
